25a698a1ba16f2a2106a2140d65468640e6485de
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-27 15:11:13 +0700

    :sparkles: Expose display functions for commonjs

--- packages/vue-pack/src/index.ts
+++ packages/vue-pack/src/index.ts
@@ -1,3 +1,6 @@
-import bundle from './bundle';
+import bundle, { displayError, displaySuccess } from './bundle';
 
 export = bundle;
+
+(bundle as any).displayError = displayError;
+(bundle as any).displaySuccess = displaySuccess;
