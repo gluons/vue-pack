e984ff4b2c578247fd3004479b95d40322347921
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-29 18:53:51 +0700

    :fire: Remove index

--- packages/vue-pack/src/index.ts
+++
@@ -1,8 +0,0 @@
-import bundle, { displayError, displaySuccess, fulfilConfig, validateConfig } from './bundle';
-
-export = bundle;
-
-(bundle as any).displayError = displayError;
-(bundle as any).displaySuccess = displaySuccess;
-(bundle as any).fulfilConfig = fulfilConfig;
-(bundle as any).validateConfig = validateConfig;
