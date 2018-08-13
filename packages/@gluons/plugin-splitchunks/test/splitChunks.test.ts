a4e2e7b4094bba14a908a7e09913455b53a8e746
Saran Tanpituckpong <sarunta@gmail.com>
2018-08-13 19:27:06 +0700

    :hankey: Fix incorrect webpack config

--- packages/@gluons/plugin-splitchunks/test/splitChunks.test.ts
+++ packages/@gluons/plugin-splitchunks/test/splitChunks.test.ts
@@ -1,38 +1,40 @@
 import { Configuration as VuePackConfig } from '@gluons/vue-pack';
 import createConfigs from '@gluons/vue-pack/dist/createConfigs';
 import { Configuration } from 'webpack';
-import Config from 'webpack-chain';
 
 import SplitChunksPlugin from '../dist';
 
-const tapAll = splitChunks => Object.assign({}, splitChunks.cacheGroups, {
-	vendors: {
-		test: /vendors1/
+const tapAll = splitChunks => Object.assign({}, splitChunks, {
+	cacheGroups: {
+		vendors: {
+			test: /vendors1/
+		}
 	}
 });
 
-const tapCJS = splitChunks => Object.assign({}, splitChunks.cacheGroups, {
-	vendors: {
-		test: /vendors2/
+const tapCJS = splitChunks => Object.assign({}, splitChunks, {
+	cacheGroups: {
+		vendors: {
+			test: /vendors2/
+		}
 	}
 });
 
-const tapESM = splitChunks => Object.assign({}, splitChunks.cacheGroups, {
-	vendors: {
-		test: /vendors3/
+const tapESM = splitChunks => Object.assign({}, splitChunks, {
+	cacheGroups: {
+		vendors: {
+			test: /vendors3/
+		}
 	}
 });
 
-const tapWeb = splitChunks => Object.assign({}, splitChunks.cacheGroups, {
-	vendors: {
-		test: /vendors4/
+const tapWeb = splitChunks => Object.assign({}, splitChunks, {
+	cacheGroups: {
+		vendors: {
+			test: /vendors4/
+		}
 	}
 });
-
-const configForAll = new Config();
-const configForCommon = new Config();
-const configForESM = new Config();
-const configForWeb = new Config();
 
 describe('SplitChunksPlugin', () => {
 	it('should change `splitChunks` in all configs', async () => {
@@ -51,8 +53,10 @@ describe('SplitChunksPlugin', () => {
 		for (let config of configs) {
 			expect(config.optimization).toHaveProperty('splitChunks');
 			expect(config.optimization.splitChunks).toMatchObject({
-				vendors: {
-					test: /vendors1/
+				cacheGroups: {
+					vendors: {
+						test: /vendors1/
+					}
 				}
 			});
 		}
@@ -78,14 +82,18 @@ describe('SplitChunksPlugin', () => {
 
 			if (i === 0) {
 				expect(config.optimization.splitChunks).toMatchObject({
-					vendors: {
-						test: /vendors2/
+					cacheGroups: {
+						vendors: {
+							test: /vendors2/
+						}
 					}
 				});
 			} else {
 				expect(config.optimization.splitChunks).not.toMatchObject({
-					vendors: {
-						test: /vendors2/
+					cacheGroups: {
+						vendors: {
+							test: /vendors2/
+						}
 					}
 				});
 			}
@@ -112,14 +120,18 @@ describe('SplitChunksPlugin', () => {
 
 			if (i === 1) {
 				expect(config.optimization.splitChunks).toMatchObject({
-					vendors: {
-						test: /vendors3/
+					cacheGroups: {
+						vendors: {
+							test: /vendors3/
+						}
 					}
 				});
 			} else {
 				expect(config.optimization.splitChunks).not.toMatchObject({
-					vendors: {
-						test: /vendors3/
+					cacheGroups: {
+						vendors: {
+							test: /vendors3/
+						}
 					}
 				});
 			}
@@ -146,14 +158,18 @@ describe('SplitChunksPlugin', () => {
 
 			if ((i === 2) || (i === 3)) {
 				expect(config.optimization.splitChunks).toMatchObject({
-					vendors: {
-						test: /vendors4/
+					cacheGroups: {
+						vendors: {
+							test: /vendors4/
+						}
 					}
 				});
 			} else {
 				expect(config.optimization.splitChunks).not.toMatchObject({
-					vendors: {
-						test: /vendors4/
+					cacheGroups: {
+						vendors: {
+							test: /vendors4/
+						}
 					}
 				});
 			}
