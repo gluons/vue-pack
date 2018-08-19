4b5b481daa47552fe5020f0687df02c4b1de9b8d
Saran Tanpituckpong <sarunta@gmail.com>
2018-08-19 19:21:11 +0700

    :children_crossing: Improve error displaying

--- packages/@gluons/cli/src/utils/displayError.ts
+++ packages/@gluons/cli/src/utils/displayError.ts
@@ -2,6 +2,8 @@ import AggregateError from 'aggregate-error';
 import chalk from 'chalk';
 import { EOL } from 'os';
 
+import extractMsgFromStack from './extractMsgFromStack';
+
 /**
  * Display error.
  *
@@ -9,17 +11,27 @@ import { EOL } from 'os';
  * @param {(Error | AggregateError)} error An error.
  */
 export default function displayError(error: Error | AggregateError): void {
-	const ERROR_TEXT = chalk.reset.inverse.bold.red(' Error ');
+	const ERROR_TEXT = chalk.reset.inverse.bold.red(' ERROR ');
+	const errors = [];
 
 	if (error instanceof AggregateError) {
 		const aggregateError: AggregateError = error;
 
 		for (const err of aggregateError) {
-			const errMsg = chalk.red(err.stack);
-			process.stderr.write(`${ERROR_TEXT}${EOL}${errMsg}${EOL}`);
+			const { msg, stack } = extractMsgFromStack(err);
+			const errMsg = chalk.red(msg);
+			const errStack = chalk.grey(stack);
+
+			errors.push(`${ERROR_TEXT}${EOL}${errMsg}${EOL}${errStack}`);
 		}
 	} else {
-		const errMsg = chalk.red(error.stack);
-		process.stderr.write(`${ERROR_TEXT}${EOL}${errMsg}${EOL}`);
+		const { msg, stack } = extractMsgFromStack(error);
+		const errMsg = chalk.red(msg);
+		const errStack = chalk.grey(stack);
+
+		errors.push(`${ERROR_TEXT}${EOL}${errMsg}${EOL}${errStack}`);
 	}
+
+	const errorStr = errors.join(EOL.repeat(2));
+	process.stderr.write(`${errorStr}${EOL}`);
 }
---
+++ packages/@gluons/cli/src/utils/extractMsgFromStack.ts
@@ -0,0 +1,38 @@
+const isMsg = (line: string) => /^\S+/.test(line);
+
+/**
+ * Extract error message (and stack) from whole stack.
+ *
+ * @export
+ * @template T Error type
+ * @param {T} err Error
+ * @returns {{ msg: string, stack: string }}
+ */
+export default function extractMsgFromStack<T extends Error>(err: T): { msg: string, stack: string } {
+	const stackLines = err.stack.split('\n');
+
+	const msgs = [];
+	const stacks = [];
+	let lineIndex = 0;
+
+	// Use the beginning of lines as message
+	while (lineIndex < stackLines.length) {
+		if (!isMsg(stackLines[lineIndex])) {
+			break;
+		}
+
+		msgs.push(stackLines[lineIndex]);
+		lineIndex++;
+	}
+
+	// Other lines are stack.
+	while (lineIndex < stackLines.length) {
+		stacks.push(stackLines[lineIndex]);
+		lineIndex++;
+	}
+
+	return {
+		msg: msgs.join('\n'),
+		stack: stacks.join('\n')
+	};
+}
